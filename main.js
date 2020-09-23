2b584408a2628dbd0b76e953ed11def34891e9d8
Sara MEBROUKA <sara.mebrouka@proxeem.fr>
2020-09-23 11:21:28 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+const {app,BrowserWindow} = require('electron')
+let mainWindow;
+
+function createWindow () {
+
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  mainWindow.loadURL('http://localhost:3000')  //ADD THIS
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('resize', function(e,x,y){
+  mainWindow.setSize(x, y);
+});
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
